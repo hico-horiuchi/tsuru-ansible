cf565cbfb8847a0145fd4854821fb55f94b72bd8
Akihiko Horiuchi <12ff5b8@gmail.com>
2018-01-04 13:54:48 +0900

    :up: update docker role for ce

--- roles/docker/spec/prerequisites_spec.rb
+++ roles/docker/spec/prerequisites_spec.rb
@@ -1,19 +1,21 @@
 require 'spec_helper'
 
-['apt-transport-https', 'ca-certificates', "linux-image-extra-#{host_inventory['kernel']['release']}"].each do |item|
+UBUNTU_RELEASE_CODE_NAME = { '16.04' => 'xenial' }.freeze
+
+%w(apt-transport-https ca-certificates curl software-properties-common).each do |item|
   describe package(item) do
      it { should be_installed }
   end
 end
 
 describe command('apt-key list | grep docker') do
-  its(:stdout) { should contain 'Docker Release Tool' }
+  its(:stdout) { should contain 'Docker Release \(CE deb\)' }
   its(:exit_status) { should eq 0 }
 end
 
-describe file('/etc/apt/sources.list.d/docker.list') do
+describe file('/etc/apt/sources.list.d/download_docker_com_linux_ubuntu.list') do
   it { should be_file }
-  it { should contain "deb https://apt.dockerproject.org/repo #{host_inventory['platform']}" }
+  it { should contain "deb [arch=amd64] https://download.docker.com/linux/ubuntu #{UBUNTU_RELEASE_CODE_NAME[host_inventory['platform_version']]} stable" }
   it { should be_mode 644 }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }

ec8c87baa62befe770a79bb55cb93df844eb98a3
Akihiko Horiuchi <12ff5b8@gmail.com>
2018-02-15 20:59:29 +0900

    :recycle: change let's encrypt port from 443 to 80 / remove cronjob for let's encrypt certification renewal

--- roles/letsencrypt/spec/cron_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe cron do
-  it { should have_entry("@weekly docker run --log-driver journald --name letsencrypt -p #{property['letsencrypt']['port']}:#{property['letsencrypt']['port']} --rm -v /etc/letsencrypt:/etc/letsencrypt -v /var/lib/letsencrypt:/var/lib/letsencrypt certbot/certbot renew").with_user('root') }
-end
